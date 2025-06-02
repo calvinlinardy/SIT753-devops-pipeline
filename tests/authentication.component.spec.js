029a2e8418ab11cc93bda2e800485ed48bec2a17
calvinlinardy <calvin.linardy@yahoo.com>
2025-06-02 23:10:40 +1000

    Fix: corrected test file syntax and assertion method

--- tests/authentication.component.spec.js
+++ tests/authentication.component.spec.js
@@ -1,36 +1,36 @@
-const assert = require('assert)')
+const assert = require('assert')
 
 describe('Component Tests', () => {
   describe('PasswordComponent', () => {
-
-    let comp
-    let service
+    let comp;
+    let service;
 
     test('should show error if passwords do not match', () => {
       // GIVEN
       comp.password = 'password1';
       comp.confirmPassword = 'password2';
+
       // WHEN
       comp.changePassword();
+
       // THEN
-      assert(comp.doNotMatch).toBe('ERROR');
-      assert(comp.error).toBeNull();
-      assert(comp.success).toBeNull();
+      expect(comp.doNotMatch).toBe('ERROR');
+      expect(comp.error).toBeNull();
+      expect(comp.success).toBeNull();
     });
 
     test('should call Auth.changePassword when passwords match', () => {
       // GIVEN
-      // deepcode ignore NoHardcodedPasswords/test: <please specify a reason of ignoring this>
       comp.password = comp.confirmPassword = 'myPassword';
 
       // WHEN
       comp.changePassword();
 
       // THEN
-      assert(service.save).toHaveBeenCalledWith('myPassword');
+      expect(service.save).toHaveBeenCalledWith('myPassword');
     });
 
-    test('should set success to OK upon success', function() {
+    test('should set success to OK upon success', () => {
       // GIVEN
       comp.password = comp.confirmPassword = 'myPassword';
 
@@ -43,7 +43,7 @@ describe('Component Tests', () => {
       expect(comp.success).toBe('OK');
     });
 
-    test('should notify of error if change password fails', function() {
+    test('should notify of error if change password fails', () => {
       // GIVEN
       comp.password = comp.confirmPassword = 'myPassword';
 
@@ -51,9 +51,9 @@ describe('Component Tests', () => {
       comp.changePassword();
 
       // THEN
-      assert(comp.doNotMatch).toBeNull();
-      assert(comp.success).toBeNull();
-      assert(comp.error).toBe('ERROR');
+      expect(comp.doNotMatch).toBeNull();
+      expect(comp.success).toBeNull();
+      expect(comp.error).toBe('ERROR');
     });
   });
 });
